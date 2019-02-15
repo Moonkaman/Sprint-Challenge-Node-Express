edcc0e5443b1638b81c871310309e7534e519ce1
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-15 10:23:52 -0700

    Added put and delete request

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -32,4 +32,32 @@ router.get('/:id', (req, res) => {
     .catch(err => res.status(500).json({errorMessage: 'Could not retrieve the project with the specified ID at this time', error: err}));
 })
 
+router.put('/:id', (req, res) => {
+  if(!req.body.name || !req.body.description) {
+    res.status(400).json({errorMessage: 'Please provide a name and description.'})
+  } else {
+    db.update(req.params.id, req.body)
+      .then(updatedProj => {
+        if(updatedProj) {
+          res.status(200).json(updatedProj)
+        } else {
+          res.status(404).json({errorMessage: 'The project with the specified ID could not be found.'})
+        }
+      })
+      .catch(err => res.status(500).json({errorMessage: 'Could not update the project with the specified id at this time', error: err}))
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(count => {
+      if(count > 0) {
+        res.status(200).json({message: `Succesfully deleted ${count} project.`})
+      } else {
+        res.status(404).json({errorMessage: 'The project with the specified ID could not be found'});
+      }
+    })
+    .catch(err => res.status(500).json({errorMessage: 'Could not delete the project with the specified ID at this time', error: err}));
+})
+
 module.exports = router;

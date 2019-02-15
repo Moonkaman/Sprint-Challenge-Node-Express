4e0d06c282d1a5ceea5a3c1e329e2fa4a7807e77
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-15 09:34:08 -0700

    Setup package.json script & base server with get req at '/'

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
-// code away!
+const server = express();
+
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.get('/', (req, res) => {
+  res.status(200).send('Welcome to the projects sprint API');
+})
+
+server.listen(8000, _ => console.log('\nServer is running on port 8000\n'))
